0d13043f194348d0150736fe4ed72ba080822868
Ludovic Ferra <57104517+ludovicferra@users.noreply.github.com>
2022-01-06 13:35:23 -0500

    Script animation

---
+++ Get-BonneFete.ps1
@@ -0,0 +1,24 @@
+Function Get-BonneFete {
+    param( [Parameter(Mandatory=$false)][String]$Prenom = "Toi" )
+    $Message ="Bonne Fête $Prenom !"
+    $Anim = @("|","/","-","\","|")
+    $CharArray =$Message.ToCharArray()
+    $i = 10
+    Clear-Host
+    Write-Host "$("#" * ($CharArray.Count + 4))"
+    Write-Host "# " -NoNewline
+    foreach ($Car in $CharArray) {
+        Do {
+            foreach ($Item in $Anim) {
+                $rounded = [math]::Round($i/10);
+                Write-Host ("$Item" * $rounded+"`b" * $rounded) -NoNewline  
+                Start-Sleep -m 50
+                $i ++
+            }
+        } While ( $i -eq $Anim.Count )
+        $i = 10
+        Write-Host $Car -NoNewline
+    }
+    Write-Host " #"
+    Write-Host "$("#" * ($CharArray.Count + 4))"
+}
